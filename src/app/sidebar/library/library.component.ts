227703ca97670efbf16205e6ebac88659e3cfe77
richardHaggioGwati <haggiog0844@gmail.com>
2023-10-03 16:49:26 +0200

    feat(sidebar): semi completed

--- src/app/sidebar/library/library.component.ts
+++ src/app/sidebar/library/library.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { faMusic } from '@fortawesome/free-solid-svg-icons';
+import { faMusic, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-library',
@@ -8,6 +8,8 @@ import { faMusic } from '@fortawesome/free-solid-svg-icons';
 export class LibraryComponent {
   //TODO: proper music list icon
   faMusicLibrary = faMusic;
+
+  faPlus = faPlus;
 
   //TODO: add a handle upload function, for user songs
 }
